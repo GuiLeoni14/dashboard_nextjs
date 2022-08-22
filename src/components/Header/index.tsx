ab671910f7e65525eda3f737c8c6fa07d34e8ece
GuiLeoni14 <guileone167@gmail.com>
2022-08-22 19:00:28 -0300

    adicionando contexto de drawer sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,31 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenu3Line } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+    const { onOpen } = useSidebarDrawer();
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     });
     return (
         <Flex w="100%" as="header" maxWidth={1480} h="20" mx="auto" mt="4" align="center" px="6">
+            {!isWideVersion && (
+                <IconButton
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenu3Line} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                    display="inline-flex"
+                />
+            )}
             <Logo />
             {isWideVersion && <SearchBox />}
             <SearchBox />
