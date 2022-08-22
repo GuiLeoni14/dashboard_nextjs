fa6c22cbc22633533bbb2a0dfad8152969056c1c
GuiLeoni14 <guileone167@gmail.com>
2022-08-22 18:29:44 -0300

    header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,19 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Guilherme Leoni</Text>
-                <Text color="gray.300" fontSize="small">
-                    guileonidev@gmail.com
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Guilherme Leoni</Text>
+                    <Text color="gray.300" fontSize="small">
+                        guileonidev@gmail.com
+                    </Text>
+                </Box>
+            )}
             <Avatar size="md" name="Guilherme Leoni" src="https://github.com/GuiLeoni14.png"></Avatar>
         </Flex>
     );
